6d9aff8dcb23ec1c5f2bfe6cd4601939a2826e2d
Jozef van Eenbergen <jozef@niuu.world>
2026-04-17 21:53:56 -0400

    Revert "feat(NIU-620): add greeting utility function and tests"
    This reverts commit 5d3c314fe6b3bfc410bd5d5b400f4005c8e85d01.

--- tests/test_greeting.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.niuu.utils.greeting import greet
-
-
-def test_greet():
-    """Test the greet function with various inputs."""
-    assert greet("World") == "Hello, World!"
-    assert greet("") == "Hello, !"
